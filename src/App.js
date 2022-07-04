3e6b257c58cf15925dfdeaf072830745d23d7f31
AudreyFW <98391306+AudreyFW@users.noreply.github.com>
2022-07-04 10:00:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import UserList from './pages/UserList';
 
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element ={<Home/>}></Route>
         <Route path='/favourite' element ={<UserList/>}></Route>
-        <Route path='*' element ={<Home />}></Route>
+        <Route path="*" element={<Navigate to="/" replace />}></Route>
       </Routes>
     </BrowserRouter>
   );
